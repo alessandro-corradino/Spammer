8190f309661952865e55a42b76706ee54176fb67
alessandro-corradino <corradino.alessandro@iisgalvanimi.edu.it>
2020-01-24 12:39:14 +0000

    EsSpammer

---
+++ prima-app/src/app/frase/frase.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-frase',
+  templateUrl: './frase.component.html',
+  styleUrls: ['./frase.component.css']
+})
+export class FraseComponent implements OnInit {
+@Input() frase: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
